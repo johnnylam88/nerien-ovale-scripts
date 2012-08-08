1f901eab940a1f9fbf450de17ba7fc99e964d667
Johnny C. Lam <jlam88@gmail.com>
2012-08-07 22:43:47 -0400

    Add the necessary glue files to turn this project into a WoW addon to be hosted on Curse.com.

---
+++ Core.lua
@@ -0,0 +1,49 @@
+NerienOvaleScripts = LibStub("AceAddon-3.0"):NewAddon("NerienOvaleScripts")
+
+NerienOvaleScripts.script = { }
+
+-- Return a deep copy of a table, preserving metatables.
+local function deepCopy(t)
+	if type(t) ~= "table" then return t end
+	local mt = getmetatable(t)
+	local result = { }
+	for k,v in pairs(t) do
+		if type(v) == "table" then
+			v = deepCopy(v)
+		end
+		result[k] = v
+	end
+	setmetatable(result, mt)
+	return result
+end
+
+-- This function touches deeply into Ovale table internals to change the default
+-- code used as well as forcing Ovale to recompile the code.
+--
+local ovaleHookRun = false
+function NerienOvaleScripts:OvaleHook()
+	-- Ensure this function only runs once.
+	if ovaleHookRun then return end
+
+	-- Ensure this function only executes after Ovale has set its own defaults.
+	local Ovale = _G.Ovale
+	if not Ovale or type(Ovale) ~= "table" then return end
+	if not Ovale.firstInit then return end
+
+	-- Make a copy of the old defaults table for Ovale.db, change the default code string,
+	-- then set that as the new defaults table for Ovale.db.
+	local defaultDB = deepCopy(Ovale.db.defaults.profile)
+	local _, classToken = UnitClass("player")
+	defaultDB.code = self.script[classToken]
+	Ovale.db:RegisterDefaults({ profile = defaultDB })
+
+	-- Force a recompile of the code with the new code string.
+	if Ovale.db.profile.code then
+		Ovale.needCompile = true
+	end
+	ovaleHookRun = true
+end
+
+function NerienOvaleScripts:OnEnable()
+	self:OvaleHook()
+end
