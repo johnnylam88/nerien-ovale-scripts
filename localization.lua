a253e7a33a64584750ed0df83a2ce9f73361ff0c
Johnny C. Lam <jlam88@gmail.com>
2020-12-07 14:11:43 -0500

    Add localization framework for adding localized text to scripts.

---
+++ localization.lua
@@ -0,0 +1,20 @@
+local _, Private = ...
+
+if Private.initialized then
+	local localization = LibStub:GetLibrary("ovale/ui/Localization")
+	local L = localization and localization.L or {}
+	local locale = GetLocale()
+
+	--[[
+	--	Insert additional localization strings into the Ovale localization
+	--	table. These symbol-to-string translations are used in the scripts
+	--	with the L() conditions, e.g., L(dispel), etc., for displaying the
+	--	correct string for text parameters.
+	--]]
+
+	-- Default locale (enUS)
+	L.dispel = [[Dispels]]
+	--L.interrupt = [[Interrupts]]
+	--L.not_in_melee_range = [[Not in melee range]]
+	L.trinkets = [[Trinkets]]
+end
