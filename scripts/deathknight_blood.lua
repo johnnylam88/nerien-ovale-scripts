0194665d3840450af07e8598e2f105910588cf5c
Johnny C. Lam <jlam88@gmail.com>
2020-12-21 00:10:33 -0500

    deathknight/blood: Add Raise Dead and Sacrificial Pact.

--- scripts/deathknight_blood.lua
+++ scripts/deathknight_blood.lua
@@ -106,8 +106,12 @@ Define(marrowrend 195182)
 Define(mind_freeze 47528)
 	SpellInfo(mind_freeze cd=15 gcd=0 offgcd=1 interrupt=1)
 Define(ossuary 219786)
+Define(raise_dead 46585)
+	SpellInfo(raise_dead cd=120 duration=60 gcd=0 offgcd=1)
 Define(rune_tap 194679)
 	SpellInfo(rune_tap cd=25 offgcd=1 runes=1 runicpower=-10 duration=4)
+Define(sacrificial_pact 327574)
+	SpellInfo(sacrificial_pact runicpower=20 cd=120)
 Define(tombstone 219809)
 	SpellInfo(tombstone cd=60)
 	SpellRequire(tombstone unusable set=1 enabled=(not HasTalent(tombstone_talent)))
@@ -292,6 +296,7 @@ AddFunction BloodDefaultMainActions
 
 AddFunction BloodPrecombatCdActions
 {
+	Spell(raise_dead)
 	Spell(dancing_rune_weapon)
 }
 
@@ -302,6 +307,12 @@ AddFunction BloodDefaultCdActions
 	if (BuffStacks(bone_shield) >= 6) Spell(tombstone)
 
 	Spell(dancing_rune_weapon)
+	if BuffExpires(dancing_rune_weapon_buff)
+	{
+		Spell(raise_dead)
+		# Sacrifice ghoul with at least 5 enemies or 15 seconds left.
+		if (Enemies() >= 5 or (SpellCooldown(raise_dead) > 60 and SpellCooldown(raise_dead) < 75)) Spell(sacrificial_pact)
+	}
 	Spell(vampiric_blood)
 	Spell(icebound_fortitude)
 	if (IncomingMagicDamage(1.5) > 0) Spell(antimagic_zone)
